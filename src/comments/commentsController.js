09e051e4061d9fda8e774826f508142431c4a631
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2023-05-23 14:31:48 -0300

    [UPDATE] Comments

--- src/comments/commentsController.js
+++ src/comments/commentsController.js
@@ -1,101 +1,101 @@
-require("./Comments");
-const mongoose = require("mongoose");
-const Comments = mongoose.model("Comments");
-const auth = require("../auth/authController");
+require('./Comments');
+const mongoose = require('mongoose');
+const Comments = mongoose.model('comments');
+const auth = require('../auth/authController');
 
 module.exports = {
-    async indexAll(req, res) {
-        let { id, token } = req.headers;
+	async indexAll(req, res) {
+		let { id, token } = req.headers;
 
-        const authorized = await auth.checkAccess(id, token);
+		const authorized = await auth.checkAccess(id, token);
 
-        if (authorized) {
-            Comments.find({})
-                .then((data) => {
-                    res.send(data);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        } else {
-            res.status(401).send("Não autorizado.");
-        }
-    },
-    async add(req, res) {
-        const { userid, name, rating, comment } = req.body;
+		if (authorized) {
+			Comments.find({})
+				.then((data) => {
+					res.send(data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		} else {
+			res.status(401).send('Não autorizado.');
+		}
+	},
+	async add(req, res) {
+		const { userid, name, rating, comment } = req.body;
 
-        try {
-            const Comments = new Comments({
-                userid: userid,
-                name: name,
-                rating: rating,
-                comment: comment,
-            });
-            Comments.save()
-                .then((data) => {
-                    console.log(data);
-                    res.status(commentData.statusCode).send(commentData.result);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        } catch (err) {
-            res.status(err.statusCode).send(err.result);
-        }
-    },
+		try {
+			const Comments = new Comments({
+				userid: userid,
+				name: name,
+				rating: rating,
+				comment: comment,
+			});
+			Comments.save()
+				.then((data) => {
+					console.log(data);
+					res.status(commentData.statusCode).send(commentData.result);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		} catch (err) {
+			res.status(err.statusCode).send(err.result);
+		}
+	},
 
-    async indexOne(req, res) {
-        let { id, token } = req.headers;
+	async indexOne(req, res) {
+		let { id, token } = req.headers;
 
-        const authorized = await auth.checkAccess(id, token);
+		const authorized = await auth.checkAccess(id, token);
 
-        if (authorized) {
-            Comments.findOne({ _id: req.params.id })
-                .then((data) => {
-                    console.log(data);
-                    res.send(data);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        } else {
-            res.status(401).send("Não autorizado!");
-        }
-    },
-    async update(req, res) {
-        let { id, token } = req.headers;
+		if (authorized) {
+			Comments.findOne({ _id: req.params.id })
+				.then((data) => {
+					console.log(data);
+					res.send(data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		} else {
+			res.status(401).send('Não autorizado!');
+		}
+	},
+	async update(req, res) {
+		let { id, token } = req.headers;
 
-        const authorized = await auth.checkAccess(id, token);
-        const update = req.body.update;
-        if (authorized) {
-            Comments.findByIdAndUpdate({ _id: req.params.id }, update)
-                .then((data) => {
-                    console.log(data);
-                    res.send(data);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        } else {
-            res.status(401).send("Não autorizado");
-        }
-    },
-    async deleteById(req, res) {
-        let { id, token } = req.headers;
+		const authorized = await auth.checkAccess(id, token);
+		const update = req.body.update;
+		if (authorized) {
+			Comments.findByIdAndUpdate({ _id: req.params.id }, update)
+				.then((data) => {
+					console.log(data);
+					res.send(data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		} else {
+			res.status(401).send('Não autorizado');
+		}
+	},
+	async deleteById(req, res) {
+		let { id, token } = req.headers;
 
-        const authorized = await auth.checkAccess(id, token);
+		const authorized = await auth.checkAccess(id, token);
 
-        if (authorized) {
-            Comments.findByIdAndRemove(req.params.id)
-                .then((data) => {
-                    console.log(data);
-                    res.send(data);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        } else {
-            res.status(401).send("Não autorizado");
-        }
-    },
+		if (authorized) {
+			Comments.findByIdAndRemove(req.params.id)
+				.then((data) => {
+					console.log(data);
+					res.send(data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		} else {
+			res.status(401).send('Não autorizado');
+		}
+	},
 };
